4393dbfb1fdfeaa674fe875d0a7fd7b506026b66
Toussaint23 <36868582+Toussaint23@users.noreply.github.com>
2018-06-27 15:08:42 -0500

    lab 2

---
+++ lab2/second.js
@@ -0,0 +1,28 @@
+Array.prototype.even = function (){
+                    let even1 = Array();
+                    let context = this;
+                    process.nextTick(function(){
+                        for(let i = 0; i < context.length; i++){
+                            if(context[i] %2 == 0)
+                            even1.push(context[i]);
+                        }
+                        console.log(even1);
+                    });
+                }
+                
+Array.prototype.odd = function (){
+    let odd1 = Array();
+    let context = this;
+    process.nextTick(function(){
+        for(let i = 0; i < context.length; i++){
+            if(context[i] %2 != 0)
+            odd1.push(context[i]);
+        }
+        console.log(odd1);
+    });
+}
+                  
+console.log("start");
+[1,2,3,4,5,6,7,8].even();
+[1,2,3,4,5,6,7,8].odd();
+console.log("end");
